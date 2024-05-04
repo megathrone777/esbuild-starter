85927a34488b722b0969434279aa41d1cfa135a6
Andrii Taranenko <megathrone3333@gmail.com>
2024-05-04 13:30:36 +0200

    Some

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import { StyledWrapper } from "./styled";
 
-const Header: React.FC = () => <StyledWrapper>Header</StyledWrapper>;
+const Header: React.FC = () => <StyledWrapper>Header2</StyledWrapper>;
 
 export { Header };
